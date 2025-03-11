980f3f99038e3674ff7ddacabccffd86333f4596
MatteoGenovese <matteo.genovese@icloud.com>
2025-03-11 20:22:51 +0100

    one way more to do carousel implementation

--- index.js
+++ index.js
@@ -12,36 +12,31 @@ for (let i = 0; i < imageList.length; i++) {
     const element = document.createElement("img");
     element.setAttribute("src", imageList[i]);
     imageContainer.append(element);
-    element.classList.add("d-none");
 }
 
 let index = 0;
 let selectedImage = imageContainer.children[index];
-selectedImage.classList.remove("d-none");
-selectedImage.classList.add("d-block");
+selectedImage.classList.toggle("active");
 
 nextBtn.addEventListener("click", function () {
-    selectedImage.classList.remove("d-block");
-    selectedImage.classList.add("d-none");
+    selectedImage.classList.toggle("active");
+
     if (index != imageContainer.children.length - 1) {
         index++;
     } else {
         index = 0;
     }
     selectedImage = imageContainer.children[index];
-    selectedImage.classList.remove("d-none");
-    selectedImage.classList.add("d-block");
+    selectedImage.classList.toggle("active");
 });
 
 prevBtn.addEventListener("click", function () {
-    selectedImage.classList.remove("d-block");
-    selectedImage.classList.add("d-none");
+    selectedImage.classList.toggle("active");
     if (index != 0) {
         index--;
     } else {
         index = imageContainer.children.length - 1;
     }
     selectedImage = imageContainer.children[index];
-    selectedImage.classList.remove("d-none");
-    selectedImage.classList.add("d-block");
+    selectedImage.classList.toggle("active");
 });
